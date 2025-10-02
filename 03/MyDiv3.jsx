1b222a7055619bbecdbf419dcfb7d203995bde25
leeyc2014 <leeyc20145@naver.com>
2025-10-02 13:33:06 +0900

    React props, useState

--- 03/MyDiv3.jsx
+++ 03/MyDiv3.jsx
@@ -1,8 +1,10 @@
 
-export default function MyDiv3() {
+// export default function MyDiv3(props) {
+export default function MyDiv3({dv1, dv2, dv3}) {
   return (
-    <div className="flex flex-col justify-center items-center h-50 w-100 bg-lime-500 text-white text-3xl font-bold text-">
-        {'div1 > div2 > div3'}
+    <div className="flex flex-col justify-start items-start p-10 m-10 h-9/10 w-9/10 bg-blue-500 text-white text-2xl font-bold text-left">
+        {/*<h1>{props.dv1} &gt; {props.dv2} &gt; {props.dv3}</h1>*/}
+        <h1>{dv1} &gt; {dv2} &gt; {dv3}</h1>
     </div>
   )
 }
--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-import MyDiv1 from './03/MyDiv1'
+// import Clock from '.02/Clock'
+//import MyDiv1 from './03/MyDiv1'
+//import MyDiv1_1 from './03_1/MyDiv1_1'
+import MyList from './04/MyList'
 
 function App() {
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center'>
-      <MyDiv1 />
+      {/* <MyDiv1 /> */}
+      {/* <MyDiv1_1 /> */}
+      <MyList />
     </div>
   )
 }
